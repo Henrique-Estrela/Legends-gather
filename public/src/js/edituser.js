c36e8a158b25857c4299b9db608c074c4cd486a0
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2023-12-06 15:52:09 -0300

    Up

--- public/src/js/edituser.js
+++ public/src/js/edituser.js
@@ -2,129 +2,86 @@ import * as functions from "./functions.js";
 
 var edituser = document.querySelector(".edituser");
 
+var inputfile = document.querySelector('#input1');
+
 
 edituser.addEventListener("click", function() {
-
-
-      Swal.fire({
-        showDenyButton: true,
-        background: "var(--d_color8)",
-        color:"#fff",
-        heightAuto: true,
-        html: `
-        <section class=" " id="pag1">
-        <div class="container">
-            <div class="progress-bar-container row">
-                <div class="col-1" id="barraprog">  
-                    <p class="numprog">1/3</p>
-                </div>
-                <div class="progress-bar-card col-10">
-                    <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                        <div class="progress-bar" style="width: 33%"></div>
-                    </div>
-                </div>
-                <div class="col-1 center">
-                    <button type="button" class="btnCloseFF"><a href="\public\views\home.html"><img src="\public\src\img\icon\Fechar.png" alt="" class="setaproximo"></a></button>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="grade grade1" id=" grade1">
-                        <div>
-                            <img src="/public/src/img/exemplo-avatar.png" alt="pudim" id="pudimdecria">
-                            <div id="editarimg">
-                                <img src="/public/src/img/Pena.png" alt="pena" id="pena">
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <label for="nome" class="nome">Nome:</label>
-                            <input type="text" name="nome" id="inpnome" >
-                        </div>
-                        <div class="flex">
-                            <div>
-                                <label for="raca" class="raca">Raça:</label>
-                                <input type="text" name="raca" id="inpraca" >
-                            </div>
-                            <div>
-                                <label for="level" class="level">Nível:</label>
-                                <input type="number" name="level" id="inplevel" >
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-6">
-                    <div class="grade grade2" id=" grade2">  
-                        <div class="col-12">
-                            <label for="classe" class="tex2">Classe:</label>
-                            <input type="text" name="classe" class="w-100" id="" >
-                        </div>
-                        <div class="col-12">
-                            <label for="tendencia" class="tex2">Tendência:</label>
-                            <input type="text" name="tendencia" class="w-100" id="" >
-                        </div>
-                        <div class="col-12">
-                            <label for="Antecedentes" class="tex2">Antecedentes:</label>
-                            <input type="text" name="Antecedentes" class="Antecedentes" id="" >
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-lg-6">
-
-                    <div class="grade grade3" id=" grade3">
-                        <div class="flex">
-                            <div>
-                                <label for="idade" class="tex3">Idade:</label>
-                                <input type="text" name="idade" class="inpgrad3" id="idade" >
-                            </div>
-                            <div>
-                                <label for="peso" class="tex3">Peso:</label>
-                                <input type="text" name="peso" id="" class="inpgrad3" >
-                            </div>
-                        </div>
-                        <div class="flex">
-                            <div>
-                                <label for="altura" class="tex3">Altura:</label>
-                                <input type="text" name="altura" id="altura" class="inpgrad3" >
-                            </div>
-                            <div>
-                                <label for="genero" class="tex3">Gênero:</label>
-                                <input type="text" name="genero" id="" class="inpgrad3" >
-                            </div>
-                        </div>
-                       
-                    </div>
-                </div>
-
-                <div class="center col-lg-6">
-                    <button value="0" type="button" id="nextform" class="btn2 btn-criar">Próximo</button>
-                </div>
-            </div>
-        </div>
-    </section>
-        `,
-        confirmButtonColor:"var(--d_color5)",
-        denyButtonColor:"var(--d_color5)",
-        confirmButtonText: `
-        <img src="../src/img/logoD&D.png" alt="D&D">
-      `,
-        denyButtonText: `
-        <img src="../src/img/logoCC.png" alt="Call Of Cthulhu">
-
-        `
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.location.href = "criar_personagem.html"
-        } else if (result.isDenied) {
+    
+    var valorExistente1 = '../src/img/logo-image.png';
+    var valorExistente2 = 'Informação existente 1';
+    var valorExistente3 = 'Informação existente 2';
+    var valorExistente4 = 'adsdadw';
+    
+    
+    
+    Swal.fire({
+        title: 'Editar Usuário',
+        html:'<div class="center">'+
+        '<div class="container-editimguser">'+
+            '<label for="input1" class="custom-file-label">'+
+            '<input type="file" id="input1" accept="image/*" class="custom-file-input">'+
+            '<img class="editimguser" src="'+ valorExistente1 +'" alt="Usuario-editar">'+
+            '</label>'+
+        '</div>'+
+        '<label for="nick">Nickname: </label> <input id="input2" name="nick" class="edit swal2-input" value="' + valorExistente2 + '">' +
+        
+        '<label for="email">Email: </label> <input id="input3" name="email" class="edit swal2-input" value="' + valorExistente3 + '">' +       
+        
+        '<label for="senha">Senha: </label> <input id="input4" name="senha" class="edit swal2-input" value="' + valorExistente4 + '">' 
+        
+        +'</div>'
+        ,
+        confirmButtonText: 'Salvar',
+        customClass: {
+            container: 'modal-customizado'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Salvar',
+        cancelButtonText: 'Cancelar',
+        preConfirm: () => {
+          // Aqui você pode acessar os valores dos inputs usando o JavaScript
+          const input1 = document.getElementById('input1').src;
+          const input2 = document.getElementById('input2').value;
+          const input3 = document.getElementById('input3').value;
+          const input4 = document.getElementById('input4').value;
+          
+          // Faça algo com os valores, por exemplo, exibindo-os em um alerta
           Swal.fire({
-            title: "Em Desenvolvimento",
-            background: "var(--d_color8)",
-            color: "#fff",
-            confirmButtonColor: "var(--d_color5)",
-            icon: "info",
-            iconColor: "#fff"
-          });
+              title: 'Valores dos Inputs',
+              html: `Input 1: ${input1}<br>Input 2: ${input2}<br>Input 2: ${input3}<br>Input 2: ${input4}`
+            });
         }
     });
-})
+
+  
+
+});
+
+
+// <<<< Algo a se implementar depois >>>>
+
+
+
+// let imageDataUrl;
+
+// inputfile.addEventListener('change', function(event) {
+// const input = event.target;
+// const file = input.files[0];
+
+// if (file) {
+//     const reader = new FileReader();
+
+//     reader.onload = function (e) {
+//     // Armazena a URL da imagem na variável
+//     imageDataUrl = e.target.result;
+//     // Aqui você pode usar imageDataUrl conforme necessário
+//     console.log('URL de dados da imagem:', imageDataUrl);
+
+//     valorExistente1 = imageDataUrl; 
+//     };
+
+//     // Lê o conteúdo do arquivo como um URL de dados
+//     reader.readAsDataURL(file);
+// }
+// });
+
